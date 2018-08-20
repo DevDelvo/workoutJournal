ad3faec7000ed2f670dd1d92c92b5104565f8f83
DevDelvo <kevinbdelvo@gmail.com>
2018-08-20 00:39:52 -0400

    dont remember what changes are here. going to see if my review req on github is working as well

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { wikiRouter, Route, Switch } from 'react-router-dom';
 import Calendar from './components/Calendar';
 import { LogIn } from './components/LogIn';
+import RoutineDay from './components/RoutineDay'
 
 class Routes extends Component {
  
@@ -13,6 +14,7 @@ class Routes extends Component {
                 <Route exact path="/" component={LogIn} />
                 {/* <Route exact path="/home" component={Home} /> */}
                 <Route exact path="/calendar" component={Calendar} />
+                <Route exact path="/routine" component={RoutineDay} />
             </Switch>
         )
     }
